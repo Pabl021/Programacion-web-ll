062070498ac46c413c3760f99068234eb596ed2e
Pabl021 <josepablobc21@gmail.com>
2020-06-12 22:29:49 -0600

    tarea casi lista

---
+++ Tarea 1 Formulario Semana 2/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$conex = mysqli_connect("localhost","root","","tareaformulario");
+ 
+if(!$conex){
+//echo "Falló la conexión";
+}else{
+
+}
+//echo "conectado";
+
+?>
---
+++ Tarea 1 Formulario Semana 2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    
+<form action="guardarDatos.php" method="post">
+
+<h3 style="color:purple">DATOS PERSONALES</h3>
+
+<input type="text" name="nombre" id="nombre" require placeholder="Digite su nombre"><br><br>
+<input type="text" name="apellido" id="apellido" require placeholder="Digite su apellido"><br><br>
+<input type="tel" name="telefono" id="telefono" require placeholder="Digite su teléfono"><br><br>
+<input type="text" name="correo" id="correo" require placeholder="Digite su correo"><br><br>
+
+<button style="color:purple" type="submit" >Guardar</button>
+
+</form>
+
+</body>
+</html>
