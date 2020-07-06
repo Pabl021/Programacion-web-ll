caad9628e50d9decc0e92f55da93c4e3902bf2b3
Pabl021 <josepablobc21@gmail.com>
2020-07-05 23:54:59 -0600

    login completo del proyecto, con sessiones,poo

---
+++ 1.TiendaVirtual/PHP/cliente.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+require('functions.php');
+require_once "conexion.php";
+$user = $_SESSION['user'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>cliente </h1>
+      <h1> Bienvenido <?php echo $user['nombre'] ?> </h1>
+      <a href="/logout.php">Logout</a>    
+</body>
+</html>
