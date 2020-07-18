0afdea26dac5b68af9c35ed2a99c4f77b621bf9e
Pabl021 <josepablobc21@gmail.com>
2020-07-18 16:26:36 -0600

    workshop listo

--- Semana-7/extraerInfo.php
+++ Semana-7/extraerInfo.php
@@ -1,26 +1,26 @@
-<?php
-include_once "function.php";
- ?>
+
 
 <?php
+include_once "function.php";
 
 $csv_end = "  
 ";  
 $csv_sep = ",";  
 $csv_file = "output.csv";  
 $csv="";  
+
 $sql="SELECT * from datos";  
-
-
 $c= new conectar();
-        $conexion=$c->conexion();
+$conexion=$c->conexion();
+$res=mysqli_query($conexion,$sql); 
 
-
-$res=mysqli_query($conexion,$sql);  
+//recorremos nuestra base de datos en busca de la info
 while($row=mysqli_fetch_array($res))  
 {  
+    //mostramos datos y mandados esos datos a la variable csv vacia
    $csv.=$row['id'].$csv_sep.$row['nombre'].$csv_sep.$row['apellido'].$csv_sep.$row['nacionalidad'].$csv_end;  
-}  echo $csv;
+}  
+echo $csv;
 //Generamos el csv de todos los datos  
 if (!$handle = fopen($csv_file, "w")) {  
     echo "Cannot open file";  
