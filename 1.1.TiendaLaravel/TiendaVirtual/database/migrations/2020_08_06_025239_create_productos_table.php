62875bc4905f106774da206fe6413dc7aa685393
Pabl021 <josepablobc21@gmail.com>
2020-08-11 16:23:13 -0600

    tercer commit en otro proyecto por problemas en el otro, login listo y registro listo

---
+++ 1.1.TiendaLaravel/TiendaVirtual/database/migrations/2020_08_06_025239_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre',50);
+            $table->string('descripcion',50);
+            $table->string('imagen',50);
+            $table->integer('codigo_categoria');
+            $table->integer('stock');
+            $table->integer('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
