ca9eafcac2f3cdd85e8797590d4eada55fe9e02b
Pabl021 <josepablobc21@gmail.com>
2020-08-11 20:24:59 -0600

    autenticación lista

--- 1.1.TiendaLaravel/TiendaVirtual/app/Http/Controllers/Auth/LoginController.php
+++ 1.1.TiendaLaravel/TiendaVirtual/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,22 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+public function redirectTo(){
+    $rol=Auth::user()->tipo;
+    switch ($rol) {
+        case 'cliente':
+            return 'cliente';
+            break;
+            case 'administrador':
+                return 'home';
+                break;
+        
+       
+    }
+}
+
 
     /**
      * Create a new controller instance.
